4cff04b52b8151ec3b74a283b366920da92e9372
obsidyne <obsidyne@gmail.com>
2026-03-02 16:57:40 +0530

    first runtime and to deploy

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+# import logging
+# logging.basicConfig(level=logging.INFO)
+
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+
+import os
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -5,5 +16,6 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
